8e3bda711521dbafd9a8c0f3268bd6928122429a
2019076880 <2019076880@hanyang.ac.kr>
2023-07-13 20:58:48 +0900

    initial commit

---
+++ src/main/java/com/ohot/OhotApplication.java
@@ -0,0 +1,13 @@
+package com.ohot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ohot/OhotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
